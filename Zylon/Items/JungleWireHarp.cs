d73bae55171ef7f68096f7ea2752c7f68003befe
braycoeYT <59518485+braycoeYT@users.noreply.github.com>
2020-01-04 20:23:46 -0600

    Add files via upload

---
+++ Zylon/Items/JungleWireHarp.cs
@@ -0,0 +1,49 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Zylon.Items
+{
+	public class JungleWireHarp : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+			//Tooltip.SetDefault("'The Jungle Wire Harp's sharp song can poison creatures. Not suggested for concerts.'");
+		}
+		public override void SetDefaults() 
+		{
+			item.damage = 19;
+			item.magic = true;
+			item.width = 33;
+			item.height = 33;
+			item.useTime = 13;
+			item.useAnimation = 13;
+			item.useStyle = 5;
+			item.knockBack = 0;
+			item.value = 3000;
+			item.rare = 2;
+			item.autoReuse = true;
+			item.useTurn = true;
+			item.shoot = mod.ProjectileType("PoisonSound");
+			item.shootSpeed = 6f;
+			item.noMelee = true;
+			item.mana = 4;
+			item.holdStyle = 3;
+			item.stack = 1;
+			item.crit = 1;
+			item.UseSound = SoundID.Item26;
+		}
+		public override void AddRecipes() 
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Vine, 3);
+			recipe.AddIngredient(ItemID.JungleSpores, 3);
+			recipe.AddIngredient(ItemID.Stinger, 5);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
