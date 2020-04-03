402aa9fcb30c5745a08d3a107d57f28238c0cf90
braycoeYT <59518485+braycoeYT@users.noreply.github.com>
2020-04-03 10:55:33 -0500

    Add files via upload

---
+++ RockyPotion.cs
@@ -0,0 +1,67 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Zylon.Items
+{
+	public class RockyPotion : ModItem
+	{
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Gives a ton of defense at the cost of attack and speed.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 10;
+            item.height = 14;
+            item.useStyle = ItemUseStyleID.EatingUsing;
+            item.useAnimation = 15;
+            item.useTime = 15;
+            item.useTurn = true;
+            item.UseSound = SoundID.Item3;
+            item.maxStack = 30;
+            item.consumable = true;
+            item.rare = 1;
+            item.value = 250;
+            item.buffType = mod.BuffType("Rocky");
+            item.buffTime = 21600;
+        }
+		
+		public override void AddRecipes() 
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.BottledWater);
+			recipe.AddIngredient(ItemID.StoneBlock, 50);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.AddTile(13);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+			
+			recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.BottledWater);
+			recipe.AddIngredient(ItemID.MarbleBlock, 5);
+			recipe.AddIngredient(ItemID.GraniteBlock, 5);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.AddTile(13);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+			
+			recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.BottledWater);
+			recipe.AddIngredient(ItemID.StoneBlock, 50);
+			recipe.AddTile(TileID.AlchemyTable);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+			
+			recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.BottledWater);
+			recipe.AddIngredient(ItemID.MarbleBlock, 5);
+			recipe.AddIngredient(ItemID.GraniteBlock, 5);
+			recipe.AddTile(TileID.AlchemyTable);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+    }
+}
