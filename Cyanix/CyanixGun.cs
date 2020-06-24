97133548e31d0ffe9cfbbf489a976d54e6097ca7
braycoeYT <59518485+braycoeYT@users.noreply.github.com>
2020-06-24 16:37:42 -0500

    Add files via upload

---
+++ Cyanix/CyanixGun.cs
@@ -0,0 +1,54 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Zylon.Items.Cyanix
+{
+	public class CyanixGun : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+			DisplayName.SetDefault("Cyanix Pistol");
+			Tooltip.SetDefault("50% Chance to not consume ammo");
+		}
+
+		public override void SetDefaults() 
+		{
+			item.value = 500;
+			item.useStyle = 5;
+			item.useAnimation = 11;
+			item.useTime = 11;
+			item.damage = 2;
+			item.width = 12;
+			item.height = 24;
+			item.knockBack = 1.5f;
+			item.shoot = 14;
+			item.shootSpeed = 30f;
+			item.noMelee = true;
+			item.ranged = true;
+			item.useAmmo = AmmoID.Bullet;
+			item.UseSound = SoundID.Item11;
+			item.autoReuse = false;
+			item.crit = 6;
+		}
+		
+		public override bool ConsumeAmmo(Player player)
+        {
+			if (Main.rand.NextFloat() < .5f)
+            return false;
+			else
+			return true;
+        }
+		
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(mod.ItemType("CyanixBar"), 9);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
---
+++ Dirtball/DirtyDiscus.cs
@@ -0,0 +1,35 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Zylon.Items.Dirtball
+{
+	public class DirtyDiscus : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+			Tooltip.SetDefault("'Feeling it makes you feel dirty...'\nShoots a Dirty Discus");
+		}
+
+		public override void SetDefaults() 
+		{
+			item.damage = 6;
+			item.melee = true;
+			item.width = 33;
+			item.height = 33;
+			item.useTime = 9;
+			item.useAnimation = 9;
+			item.useStyle = 4;
+			item.knockBack = 1.5f;
+			item.value = 2000;
+			item.rare = -1;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+			item.useTurn = true;
+			item.shoot = mod.ProjectileType("DirtyDiscus");
+			item.shootSpeed = 12f;
+		}
+	}
+}
