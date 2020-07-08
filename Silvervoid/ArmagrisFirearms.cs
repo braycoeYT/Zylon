283cdf18bc2f3c7cf81ee9d380529afaf29995b7
braycoeYT <59518485+braycoeYT@users.noreply.github.com>
2020-07-08 08:04:47 -0500

    Add files via upload

--- Silvervoid/ArmagrisFirearms.cs
+++ Silvervoid/ArmagrisFirearms.cs
@@ -8,21 +8,19 @@ namespace Zylon.Items.Silvervoid
 {
 	public class ArmagrisFirearms : ModItem
 	{
-		public override void SetStaticDefaults() 
-		{
+		public override void SetStaticDefaults() {
 			DisplayName.SetDefault("Armagris Firearms");
 			Tooltip.SetDefault("The pink gun's little brother\nShoots 3 Bullets for the cost of one\n45% chance of not consuming ammo");
 		}
 
-		public override void SetDefaults() 
-		{
+		public override void SetDefaults() {
 			item.value = 500000;
 			item.useStyle = 5;
 			item.useAnimation = 8;
 			item.useTime = 8;
 			item.damage = 41;
-			item.width = 12;
-			item.height = 24;
+			item.width = 60;
+			item.height = 45;
 			item.knockBack = 1.1f;
 			item.shoot = 14;
 			item.shootSpeed = 10f;
@@ -34,9 +32,10 @@ namespace Zylon.Items.Silvervoid
 			item.rare = 10;
 			item.noMelee = true;
 		}
-
-        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
-		{
+		public override Vector2? HoldoutOffset() {
+			return new Vector2(-3, 0);
+		}
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack) {
 			float numberProjectiles = 3;
 			float rotation = MathHelper.ToRadians(7);
 			position += Vector2.Normalize(new Vector2(speedX, speedY)) * 75f;
@@ -47,17 +46,14 @@ namespace Zylon.Items.Silvervoid
 			}
 			return false;
 		}
-		
-		public override bool ConsumeAmmo(Player player)
-        {
+		public override bool ConsumeAmmo(Player player) {
 			if (Main.rand.NextFloat() < .45f)
             return false;
 			else
 			return true;
         }
 		
-		public override void AddRecipes() 
-		{
+		public override void AddRecipes()  {
 			ModRecipe recipe = new ModRecipe(mod);
 			recipe.AddIngredient(mod.ItemType("SilvervoidCore"), 11);
 			recipe.AddIngredient(ItemID.SDMG);
--- Xenic/XenicCore.cs
+++ Xenic/XenicCore.cs
@@ -1,5 +1,9 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
 
 namespace Zylon.Items.Xenic
 {
@@ -10,6 +14,7 @@ namespace Zylon.Items.Xenic
 			Tooltip.SetDefault("A very compact and powerful core\nHolding this makes you feel like you are being watched...");
 			ItemID.Sets.ItemIconPulse[item.type] = true;
 			ItemID.Sets.ItemNoGravity[item.type] = true;
+			Main.RegisterItemAnimation(item.type, new DrawAnimationVertical(11, 11)); //first is speed, second is amount of frames
 		}
 
 		public override void SetDefaults() 
