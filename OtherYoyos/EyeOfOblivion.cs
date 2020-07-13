58eecb5fa493744a91cf7c2f24e3afa956d0ebc3
braycoeYT <59518485+braycoeYT@users.noreply.github.com>
2020-07-13 11:42:03 -0500

    Add files via upload

--- OtherYoyos/EyeOfOblivion.cs
+++ OtherYoyos/EyeOfOblivion.cs
@@ -1,58 +1,49 @@
-using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-using static Terraria.ModLoader.ModContent;
 
-namespace Zylon.Items.OtherYoyos
+namespace Zylon.Projectiles.OtherYoyos
 {
-	public class EyeOfOblivion : ModItem
+	public class EyeOfOblivion : ModProjectile
 	{
-		public override void SetStaticDefaults() 
+		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Eye of Oblivion");
-			Tooltip.SetDefault("Powered by the sun!\nShoots two yoyos, the second one orbits around the first\nBoth yoyos can burn enemies");
-			ItemID.Sets.Yoyo[item.type] = true;
-			ItemID.Sets.GamepadExtraRange[item.type] = 15;
-			ItemID.Sets.GamepadSmartQuickReach[item.type] = true;
+			//3-16 Vanilla, -1 = Infinite
+			ProjectileID.Sets.YoyosLifeTimeMultiplier[projectile.type] = 14f;
+			//130-400 Vanilla
+			ProjectileID.Sets.YoyosMaximumRange[projectile.type] = 375f;
+			//9-17.5 Vanilla, for future reference
+			ProjectileID.Sets.YoyosTopSpeed[projectile.type] = 16.5f;
 		}
 
 		public override void SetDefaults()
 		{
-			item.useStyle = 5;
-			item.width = 24;
-			item.height = 24;
-			item.useAnimation = 25;
-			item.useTime = 25;
-			item.shootSpeed = 16f;
-			item.knockBack = 4f;
-			item.damage = 145;
-			item.rare = 9;
-			item.melee = true;
-			item.channel = true;
-			item.noMelee = true;
-			item.noUseGraphic = true;
-			item.UseSound = SoundID.Item1;
-			item.value = 1000000;
-			item.shoot = ProjectileType<Projectiles.OtherYoyos.EyeOfOblivion>();
+			projectile.extraUpdates = 0;
+			projectile.width = 16;
+			projectile.height = 16;
+			projectile.aiStyle = 99;
+			projectile.friendly = true;
+			projectile.penetrate = -1;
+			projectile.melee = true;
+			projectile.scale = 1f;
 		}
-		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
 		{
-			Projectile.NewProjectile(position.X, position.Y, speedX, speedY, type, damage, knockBack, player.whoAmI);
-			Projectile.NewProjectile(position.X, position.Y, speedX * -1, speedY * -1, type, damage, knockBack, player.whoAmI);
-			return false;
+			target.AddBuff(BuffID.OnFire, 150, false);
 		}
-		public override void AddRecipes()
+		public override void OnHitPlayer(Player target, int damage, bool crit)
 		{
-			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(mod.ItemType("DirtYoyo"));
-			recipe.AddIngredient(mod.ItemType("Stalkeye"));
-			recipe.AddIngredient(mod.ItemType("TheMeatbringer"));
-			recipe.AddIngredient(ItemID.TheEyeOfCthulhu);
-			recipe.AddIngredient(ItemID.FragmentSolar, 9);
-			recipe.AddTile(TileID.LunarCraftingStation);
-			recipe.SetResult(this);
-			recipe.AddRecipe();
+			target.AddBuff(BuffID.OnFire, 150, false);
+		}
+		public override void PostAI()
+		{
+			if (Main.rand.NextBool())
+			{
+				Dust dust = Dust.NewDustDirect(projectile.position, projectile.width, projectile.height, 35);
+				dust.noGravity = true;
+				dust.scale = 1.6f;
+			}
 		}
 	}
 }
