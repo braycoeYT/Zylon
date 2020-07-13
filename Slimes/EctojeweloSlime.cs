89c18a749bf833c6791b785fe7fa553c73bf1b63
braycoeYT <59518485+braycoeYT@users.noreply.github.com>
2020-07-13 11:36:40 -0500

    Add files via upload

--- Slimes/EctojeweloSlime.cs
+++ Slimes/EctojeweloSlime.cs
@@ -16,7 +16,7 @@ namespace Zylon.NPCs.Slimes
 		{
 			npc.width = 28;
 			npc.height = 27;
-			npc.damage = 174;
+			npc.damage = 118;
 			npc.defense = 75;
 			npc.lifeMax = 11020;
 			npc.HitSound = SoundID.NPCHit1;
@@ -30,7 +30,7 @@ namespace Zylon.NPCs.Slimes
 		public override void ScaleExpertStats(int numPlayers, float bossLifeScale)
         {
             npc.lifeMax = 21485;
-            npc.damage = 271;
+            npc.damage = 195;
 			npc.defense = 100;
         }
 		public override void OnHitPlayer(Player player, int damage, bool crit)
@@ -52,7 +52,7 @@ namespace Zylon.NPCs.Slimes
 			target2.X += Main.rand.Next(-60, 60);
 			target2.Y += Main.rand.Next(-60, 60);
 			Timer++;
-			if (Timer % 120 == 0)
+			if (Timer % 250 == 0)
 				Projectile.NewProjectile(npc.Center, new Vector2(0, 6).RotatedByRandom(MathHelper.TwoPi), mod.ProjectileType("PinkGemblast"), 54, 1f, Main.myPlayer);
 		}
 		public override float SpawnChance(NPCSpawnInfo spawnInfo)
