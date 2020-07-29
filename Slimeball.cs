d3129093da5475c5ecc610e40e462485d9e94e6b
braycoeYT <59518485+braycoeYT@users.noreply.github.com>
2020-07-28 22:46:30 -0500

    Add files via upload

---
+++ Slimeball.cs
@@ -0,0 +1,47 @@
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace Zylon.Projectiles
+{
+	public class Slimeball : ModProjectile
+	{
+        public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Slimeball");
+			Main.projFrames[projectile.type] = 6;
+        }
+		public override void SetDefaults()
+		{
+			aiType = ProjectileID.Bullet;
+			projectile.width = 40;
+			projectile.height = 40;
+			projectile.aiStyle = 1;
+			projectile.hostile = false;
+			projectile.friendly = true;
+			projectile.timeLeft = 600;
+			projectile.ignoreWater = true;
+			projectile.tileCollide = Main.hardMode;
+			projectile.penetrate = 10;
+			if (NPC.downedMoonlord) {
+				projectile.scale = 1.5f;
+				projectile.width = 60;
+				projectile.height = 60;
+			}
+		}
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit) {
+		    target.AddBuff(BuffID.Slimed, 300, false);
+		}
+		public override void AI()
+		{
+			if (++projectile.frameCounter >= 6)
+			{
+				projectile.frameCounter = 0;
+				if (++projectile.frame >= 6)
+				{
+					projectile.frame = 0;
+				}
+			}
+		}
+	}   
+}
