ef63905b65e7e8eaca0f0118a877251702cc622c
braycoeYT <59518485+braycoeYT@users.noreply.github.com>
2020-08-01 19:02:53 -0500

    Add files via upload

---
+++ Eye/Stalkeye.cs
@@ -0,0 +1,40 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Zylon.Items.Eye
+{
+	public class Stalkeye : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+			DisplayName.SetDefault("Stalkeye");
+			Tooltip.SetDefault("It stares...");
+			ItemID.Sets.Yoyo[item.type] = true;
+			ItemID.Sets.GamepadExtraRange[item.type] = 15;
+			ItemID.Sets.GamepadSmartQuickReach[item.type] = true;
+		}
+
+		public override void SetDefaults()
+		{
+			item.useStyle = 5;
+			item.width = 24;
+			item.height = 24;
+			item.useAnimation = 25;
+			item.useTime = 25;
+			item.shootSpeed = 16f;
+			item.knockBack = 3f;
+			item.damage = 16;
+			item.rare = 1;
+			item.melee = true;
+			item.channel = true;
+			item.noMelee = true;
+			item.noUseGraphic = true;
+			item.UseSound = SoundID.Item1;
+			item.value = 2000;
+			item.shoot = ProjectileType<Projectiles.OtherYoyos.Stalkeye>();
+		}
+	}
+}
--- Magentite/MagentiteBlowpipe.cs
+++ Magentite/MagentiteBlowpipe.cs
@@ -10,14 +10,22 @@ namespace Zylon.Items.Magentite
 	{
 		public override void SetStaticDefaults() 
 		{
-			Tooltip.SetDefault("Uses seed as ammo");
+			Tooltip.SetDefault("Uses seed shots as ammo\nWhile in your inventory, the following enemies will drop Magentite Seedshots:\nMagentite Slime, Magentite Discus, Magentite Stinger");
 		}
 		public override void SetDefaults() 
 		{
 			item.CloneDefaults(ItemID.Blowpipe);
-			item.damage = 10;
-			item.knockBack = 3.6f;
-			item.shootSpeed = 11f;
+			item.damage = 10; //9, mod 11
+			item.knockBack = 3.3f; //3.5
+			item.shootSpeed = 10.9f; //11
+			item.useTime = 45; //45
+			item.useAnimation = 45; //45
+			item.crit = 4;
+		}
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			player.AddBuff(mod.BuffType("OutOfBreath"), item.useTime, false);
+			return true;
 		}
 		public override Vector2? HoldoutOffset() {
 			return new Vector2(0, -5);
